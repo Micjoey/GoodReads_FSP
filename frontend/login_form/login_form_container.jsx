b2ab5db70b1c99fee5529416cb2218c91bdcba4b
Macallan Savett <jsavett@gmail.com>
2020-01-06 18:50:18 -0800

    remove email user column, to make login and signup uniform

--- frontend/login_form/login_form_container.jsx
+++ frontend/login_form/login_form_container.jsx
@@ -8,6 +8,7 @@ const mapStateToProps = ({ errors }) => {
   return {
     errors: errors.session,
     formType: 'login',
+    navLink: <Link to="/signup">sign up instead</Link>,
   };
 };
 
--- frontend/login_form/signup_form_container.jsx
+++ frontend/login_form/signup_form_container.jsx
@@ -1,19 +1,21 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { login } from '../../actions/session_actions';
+import { signUp } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
 const mapStateToProps = ({ errors }) => {
+  // debugger
   return {
     errors: errors.session,
-    formType: 'login',
+    formType: 'signup',
+    navLink: <Link to="/login">log in instead</Link>,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    processForm: (user) => dispatch(login(user)),
+    processForm: (user) => dispatch(signUp(user)),
   };
 };
 
