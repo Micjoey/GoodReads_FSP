e74ccdf810d04a4304b2089107bf7846735aca6e
Macallan Savett <jsavett@gmail.com>
2020-01-07 08:48:54 -0800

    drop shelves table

--- db/migrate/20200107163917_drop_table_shelves.rb
+++ db/migrate/20200107163917_drop_table_shelves.rb
@@ -1,4 +1,5 @@
 class DropTableShelves < ActiveRecord::Migration[5.2]
   def change
+    drop_table :shelves
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_163405) do
+ActiveRecord::Schema.define(version: 2020_01_07_163917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,13 +34,6 @@ ActiveRecord::Schema.define(version: 2020_01_07_163405) do
     t.string "bookshelf_title", null: false
     t.integer "book_id", null: false
     t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shelves", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "bookshelf_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
