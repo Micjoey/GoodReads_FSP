e2d7bd75fdfe9581d1a9804431d9dae1f8312a26
Macallan Savett <jsavett@gmail.com>
2020-01-09 10:53:00 -0800

    Fixed LogOut bug ln 28

--- frontend/components/entry/entryfile.jsx
+++ frontend/components/entry/entryfile.jsx
@@ -21,11 +21,11 @@ class EntryFile extends React.Component {
         <div className="header-group">
             <Link to="/" className="myreads-title">MyReads</Link>
                 <h2 className="header-name">Hi, {this.props.currentUser.username}!</h2>
-                {/* <button className="header-button" onClick={this.props.logout}>Log Out</button> */}
+                <button className="header-button" onClick={this.props.logout}>Log Out</button>
                 <select className=".dropdown">
-                    <option>Hello</option>
+                    {/* <option>Hello</option>
                     <option onClick={this.props.logout}>Log Out</option>
-                    <option href="#" >{LogOut}</option>
+                    <option href="#" >{LogOut}</option> */}
                     
                 </select>
         </div>
