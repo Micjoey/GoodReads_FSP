289b816b10ff7c8c03c5bcce31677e6c34f61d65
Macallan Savett <jsavett@gmail.com>
2020-01-11 19:19:31 -0800

    Rendered book covers

--- frontend/components/book/index_book.jsx
+++ frontend/components/book/index_book.jsx
@@ -12,10 +12,27 @@ class IndexBook extends React.Component {
     }
 
     render() {
+        const book_cover = [
+            images.american_spy, 
+            images.book_of_strange, 
+            images.city_of_girls, 
+            images.maybe_you_should_talk_to_someone, 
+            images.quichotte, 
+            images.super_pumped, 
+            images.swanns_way,
+            images.the_names_of_the_dead,
+            images.the_nickel_boys,
+            images.the_starless_sea,
+            images.the_testaments,
+            images.they_called_us_enemy,
+            images.upright_woman_wanted,
+            images.wild_game,
+        ]
         const books = (this.props.books.length !== 0) ? (
                 <ul>
                     {this.props.books.map((book, i) => (
                         <ul className="all-books" key={`book-${i}`}>
+                            <ul><img src={book_cover[i]} className="all-book-covers" alt=""/></ul>
                             <ul>{book.title}, {book.author}</ul>
                             <ul></ul>
                                     
