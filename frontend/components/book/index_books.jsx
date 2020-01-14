5c0af236580ffdf639ef789bd00d7cd271330cc2
Macallan Savett <jsavett@gmail.com>
2020-01-13 16:07:48 -0800

    Changed Index_books

--- frontend/components/book/index_books.jsx
+++ frontend/components/book/index_books.jsx
@@ -21,29 +21,14 @@ class IndexBook extends React.Component {
     }
 
     render() {
-        const book_cover = [
-            images.american_spy, 
-            images.book_of_strange, 
-            images.upright_woman_wanted,
-            images.city_of_girls, 
-            images.maybe_you_should_talk_to_someone, 
-            images.the_names_of_the_dead,
-            images.quichotte, 
-            images.swanns_way,
-            images.super_pumped, 
-            images.the_nickel_boys,
-            images.the_starless_sea,
-            images.the_testaments,
-            images.they_called_us_enemy,
-            images.wild_game,
-        ]
+        
         if (!this.props.books) return null;
         const books = (
                 <div className="index-books">
                     {this.props.books.map((book, i) => (
                         <div key={`book-${i}`} className="index-books-book-info">
                             <Link to={`/book/${i+1}`}>
-                                <div className="index-book-covers"><img src={book_cover[i]} className="index-book-cover"/></div>
+                                <div className="index-book-covers"><img src={book.photo} className="index-book-cover"/></div>
                             </Link>
                             <div className='index-book-information-title'>{book.title}</div>
                             <div className='index-book-information-author'>by: {book.author}</div>
@@ -63,3 +48,26 @@ class IndexBook extends React.Component {
 
 
 export default IndexBook
+
+
+
+
+
+
+
+// const book_cover = [
+//     images.american_spy,
+//     images.book_of_strange,
+//     images.upright_woman_wanted,
+//     images.city_of_girls,
+//     images.maybe_you_should_talk_to_someone,
+//     images.the_names_of_the_dead,
+//     images.quichotte,
+//     images.swanns_way,
+//     images.super_pumped,
+//     images.the_nickel_boys,
+//     images.the_starless_sea,
+//     images.the_testaments,
+//     images.they_called_us_enemy,
+//     images.wild_game,
+// ]
