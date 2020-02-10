d00b3fd1aeb22e3f6514a3c9b8aeafb620c83893
Macallan Savett <jsavett@gmail.com>
2020-02-10 15:12:04 -0700

    Create unique shelves verification

--- frontend/components/book/show_book.jsx
+++ frontend/components/book/show_book.jsx
@@ -4,7 +4,6 @@ import { formatDateWithDay } from '../../util/date_util';
 import AddShelfContainer from './add_shelf_container';
 import CreateReviewContainer from '../reviews/create_review_form_container';
 import { deleteReview } from '../../actions/review_actions';
-import { retrieveBook } from '../../actions/book_actions';
 
 
 
@@ -109,12 +108,10 @@ class showBook extends React.Component {
                                             <div className="show-book-individual-review-id">User: {allUsers[review.user_id].username}</div>
                                             <div className="show-book-individual-review-rating">User Rating: {review.rating}</div>
                                             <div className="show-book-individual-review-body">Review: {review.body}</div>
-                                            {/* Need to change the below code */}
                                             <div>{(review.user_id === this.props.userId) ? <button 
                                                 onClick={() => this.handleDelete(review.id)}>
                                                     Delete Review</button> : null }
                                             </div>
-                                            {/* ----------------------------- */}
                                         </div>
                                     ))}
                             </div>
