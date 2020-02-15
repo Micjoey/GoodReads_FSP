1558064e740ade7029fe33537fb808f89059d573
Macallan Savett <jsavett@gmail.com>
2020-02-15 09:49:17 -0700

    trying to fix splash page styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LoginFormContainer from '../session_form/login_form_container';
 import SignUpFormContainer from '../session_form/signup_form_container'
 
 
@@ -18,7 +16,7 @@ class Splash extends React.Component {
                     <SignUpFormContainer />
                 </div>
                 <div className="background-box">
-                    {/* <div> Track your favorite reads here!</div> */}
+                    {/* <div className="flavor-text"> Track your favorite reads here!</div> */}
                     <img src={images.fireworks2} className="firework" alt="" />
                 </div>
             </div>
