bc92fc9953eeccc304414372f8444a0033256b13
Macallan Savett <jsavett@gmail.com>
2020-02-19 12:04:19 -0800

    add target _blank to footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -10,12 +10,12 @@ class Footer extends React.Component {
         return (
             <div className="footer-inner">
                 <a href="https://www.linkedin.com/in/macallan-savett/">
-                    <img className="footer-linkedin" src={images.linkedin} alt=""/>
+                    <img className="footer-linkedin" src={images.linkedin} alt="" target='_blank' />
                 </a>
                 <a href="https://github.com/Micjoey/MyReads_FSP/wiki">
-                    <img src={images.github} className="footer-github" alt=""/>
+                    <img src={images.github} className="footer-github" alt="" target='_blank' />
                 </a>
-                <a href="https://github.com/Micjoey" id="footerLink">©  Macallan Savett</a>
+                <a href="https://github.com/Micjoey" id="footerLink" target='_blank' >©  Macallan Savett</a>
             </div>
 
         );
