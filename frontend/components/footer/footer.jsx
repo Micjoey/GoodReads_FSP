6bbab68f91cac589c1f552f4176a79b33275079a
Macallan Savett <jsavett@gmail.com>
2020-02-21 16:32:54 -0800

    fix opening a new window from footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,11 +9,11 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footer-inner">
-                <a href="https://www.linkedin.com/in/macallan-savett/">
-                    <img className="footer-linkedin" src={images.linkedin} rel="noopener noreferrer" target="_blank" />
+                <a href="https://www.linkedin.com/in/macallan-savett/" rel="noopener noreferrer" target="_blank" >
+                    <img className="footer-linkedin" src={images.linkedin}/>
                 </a>
-                <a href="https://github.com/Micjoey/MyReads_FSP/wiki">
-                    <img src={images.github} className="footer-github" rel="noopener noreferrer" target="_blank" />
+                <a href="https://github.com/Micjoey/MyReads_FSP/wiki" rel="noopener noreferrer" target="_blank" >
+                    <img src={images.github} className="footer-github" />
                 </a>
                 <a href="https://github.com/Micjoey" id="footerLink" rel="noopener noreferrer" target="_blank" >©  Macallan Savett</a>
             </div>
