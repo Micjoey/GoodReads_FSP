c87256338be14d2e7a6c1aa0ce67e6dc8961a153
Macallan Savett <jsavett@gmail.com>
2020-02-24 15:33:03 -0800

    change github link to be to code not wiki

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                 <a href="https://www.linkedin.com/in/macallan-savett/" rel="noopener noreferrer" target="_blank" >
                     <img className="footer-linkedin" src={images.linkedin}/>
                 </a>
-                <a href="https://github.com/Micjoey/FunReads_FSP/wiki" rel="noopener noreferrer" target="_blank" >
+                <a href="https://github.com/Micjoey/FunReads_FSP" rel="noopener noreferrer" target="_blank" >
                     <img src={images.github} className="footer-github" />
                 </a>
                 <a href="https://github.com/Micjoey" id="footerLink" rel="noopener noreferrer" target='_blank'>©  Macallan Savett</a>
