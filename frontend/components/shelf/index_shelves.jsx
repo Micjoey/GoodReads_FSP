f8ca41c98c06b956c1a5adcb96148e728702381a
Macallan Savett <jsavett@gmail.com>
2020-02-25 15:20:42 -0800

    styling for filtering shelves

--- frontend/components/shelf/index_shelves.jsx
+++ frontend/components/shelf/index_shelves.jsx
@@ -29,8 +29,11 @@ class IndexShelves extends React.Component {
     // onclick filter the shelves and set a new variable
     filterShelf(shelf) {
         let shelfName = shelf
+        let styling = document.getElementById(`${shelfName}`)
+        this.props.shelves.forEach(indivShelf => document.getElementById(indivShelf.bookshelf_title).classList.remove('filtered'))
         if (shelf !== 'All Books') {
-        let newShelf = this.props.shelves.filter(indivShelf => shelfName === indivShelf.bookshelf_title)
+            let newShelf = this.props.shelves.filter(indivShelf => shelfName === indivShelf.bookshelf_title)
+            styling.classList.add('filtered')
         // debugger
         this.setState({shelf: newShelf}) } else {
             this.setState({shelf: this.props.retrieveBooks()})
@@ -59,13 +62,13 @@ class IndexShelves extends React.Component {
                         <div className="index-shelves-main-sidebar">
                             <div className="index-shelves-sidebar-list-shelves">
                                 <p className="index-shelves-sidebar-title">Bookshelves</p>
-                                <button className="index-shelves-sidebar-shelf-buttons" onClick={() => this.filterShelf('All Books')}>
+                                <button className="index-shelves-sidebar-shelf-buttons" id={`'All Books'`} onClick={() => this.filterShelf('All Books')}>
                                     All Books
                                 </button>
                                 {this.props.shelves.map((shelf, i) => (
                                     <div key={`shelf-${i}`} className="index-shelves-sidebar-shelf">
-                                        <button className="index-shelves-sidebar-shelf-buttons" onClick={()=>this.filterShelf(shelf.bookshelf_title)}>
-                                            <ul className={`index-shelves-sidebar-shelf-button-${shelf.bookshelf_title}`}>
+                                        <button className="index-shelves-sidebar-shelf-buttons" id={`${shelf.bookshelf_title}`} onClick={()=>this.filterShelf(shelf.bookshelf_title)}>
+                                            <ul className={`index-shelves-sidebar-shelf-button-${shelf.bookshelf_title}`} >
                                                 {shelf.bookshelf_title}
                                             </ul>
                                         </button>
