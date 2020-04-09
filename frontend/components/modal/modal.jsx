ba0d060b5179e10172a7fb95e10cf097c25ae363
Macallan Savett <jsavett@gmail.com>
2020-04-09 11:24:53 -0700

    comment out the import of the loggedinformcontainer

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { closeModal} from '../../actions/model_actions';
 import { connect } from 'react-redux';
-import LoggedinFormContainer from '../greeting/logged_in_container';
+// import LoggedinFormContainer from '../greeting/logged_in_container';
 import SearchBarContainer from '../navbar/search_bar_container';
 
 function Modal({modal, closeModal}) {
