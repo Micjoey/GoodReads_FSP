eac0cb672aa80e1e01627c02a491ccf39187e041
Macallan Savett <jsavett@gmail.com>
2020-04-24 11:58:09 -0700

    add filter by average ratings button

--- frontend/components/book/index_books/Index_books_filter_bar.jsx
+++ frontend/components/book/index_books/Index_books_filter_bar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Redirect, withRouter } from 'react-router-dom'
 import { IndivRating } from '../../stars/star';
 import IndexBooks from './index_books';
+import MinMaxBookRatings from '../min_max_book_rating';
 
 
 
@@ -17,7 +18,6 @@ class IndexBooksFilterBar extends React.Component {
 
         this.filterBooks = this.filterBooks.bind(this)
         this.updateState = this.updateState.bind(this)
-        this.filterBooksByGenre = this.filterBooksByGenre.bind(this)
         this.filterBooksByRating = this.filterBooksByRating.bind(this)
         this.allGenres = this.allGenres.bind(this)
         this.dropDown = this.dropDown.bind(this)
@@ -49,61 +49,67 @@ class IndexBooksFilterBar extends React.Component {
             indexBooksHTML.style.gridTemplateColumns = "auto auto auto";
             indexBookInfo.style.padding = `1%`
         }
-
         if (booksArray.length === 0) {
             indexBooksHTML.style.gridTemplateColumns = "auto";
             indexBookInfo.style.padding = `7%`
         }
+    }
 
+    filterBooks(text, idx) {
+        this.setState({ bookSearch: text }, () => this.updateState())
     }
 
     updateState() {
-        const allBooks = this.props.books.filter(indivBook =>
-            indivBook.title.toLowerCase().includes(this.state.bookSearch.toLowerCase()) ||
-            indivBook.author.toLowerCase().includes(this.state.bookSearch.toLowerCase()) ||
-            indivBook.genre.toLowerCase().includes(this.state.bookSearch.toLowerCase())
+        const searchElement = this.state.bookSearch
+        const filteredBooks = this.props.books.filter(indivBook =>
+            indivBook.title.toLowerCase().includes(searchElement.toLowerCase()) ||
+            indivBook.author.toLowerCase().includes(searchElement.toLowerCase()) ||
+            indivBook.genre.toLowerCase().includes(searchElement.toLowerCase())
         ).map(indivBook => indivBook);
 
         
 
         let notfound = images.notFound;
 
-        if (allBooks.length === 0) {
+        if (filteredBooks.length === 0) {
             this.setState({ books: [{ title: 'Not Found', photo: notfound }], bookSearch: '' })
         } else {
-            this.setState({ books: allBooks })
+            this.setState({ books: filteredBooks })
         }
-        this.dynamicGridStyling(allBooks)
+        this.dynamicGridStyling(filteredBooks)
     }
 
-    filterBooks(text) {
-        this.setState({ bookSearch: text }, () => this.updateState())
-    }
+    
 
     allGenres() {
         const indivGenres = {}
-        this.props.books.map(indivBook => indivGenres[indivBook.genre] = true)
+        this.props.books.map(indivBook => {
+            indivGenres[indivBook.genre.toLowerCase()] = true})
         return Object.keys(indivGenres)
     }
+    
 
-    filterBooksByGenre(genre) {
-        const indivGenres = {}
-        this.props.books.map(indivBook => indivGenres[indivBook.genre] = true)
-        if (genre !== 'All Books') {
-            this.filterBooks(genre)
-        } else {
-            this.filterBooks()
-        }
-
-    }
 
     filterBooksByRating(rating) {
+       
+        const leveledRating = Math.floor(parseInt(rating))
+        let filteredBooks = this.props.books
+        
+        if (rating !== '') {
+            debugger
+            filteredBooks = this.props.books.filter(indivBook =>
+                Math.floor(parseInt(indivBook.average_rating)) === leveledRating
+            ).map(indivBook => indivBook);
+        } 
+        this.setState({ books: filteredBooks })
+        this.dynamicGridStyling(filteredBooks)
+        
 
     }
 
-    dropDown() {
-        const dropDown = document.getElementById("genre-dropdown")
-        const genreTitle = document.getElementById("genre-header")
+    dropDown(dropDownId) {
+        const dropDown = document.getElementById(`${dropDownId}-dropdown`)
+        const genreTitle = document.getElementById(`${dropDownId}-header`)
         if (dropDown.style.display) {
             dropDown.style.display = null
             genreTitle.classList.toggle("filter-bar-clicked-on")
@@ -112,6 +118,19 @@ class IndexBooksFilterBar extends React.Component {
             genreTitle.classList.toggle("filter-bar-clicked-on")
         }
 
+    }
+
+
+
+    toggleColoring(shelf) {
+        let styling = document.getElementById(`${shelf.bookshelf_title}`)
+        if (styling.name === 'checked') {
+            styling.classList.remove('filtered')
+            styling.name = `not-checked`
+        } else if (styling.name === `not-checked`) {
+            styling.classList.add('filtered')
+            styling.setAttribute('name', 'checked')
+        }
     }
 
 
@@ -129,19 +148,40 @@ class IndexBooksFilterBar extends React.Component {
                         <div className='nav-bar-search'>
                             <div className="dropdown">
                                 <h3 className="filter-bar-titles" id="genre-header"
-                                    onClick={() => this.dropDown()}>Genre</h3>
+                                    onClick={() => this.dropDown("genre")}>Genre</h3>
                                 <div className='nav-bar-search-by-genre' id="genre-dropdown">
                                     <button className="index-book-filter-bar-buttons"
-                                        onClick={() => this.filterBooksByGenre(``)}>
+                                        onClick={() => this.filterBooks(``)}>
                                         All Books
                                     </button>
-                                    {this.allGenres().map(genre => (
+                                    {this.allGenres().map((genre, idx) => (
                                         <div key={`${genre}`} className="add-shelves-sidebar-shelf">
                                             <button className="index-book-filter-bar-buttons"
-                                                onClick={() => this.filterBooksByGenre(`${genre}`)}
+                                                onClick={() => this.filterBooks(`${genre}`, `${idx}`)}
                                             >
-                                                <ul className={`index-book-filter-bar-buttons`} >
+                                                <ul className={`index-book-filter-bar-buttons`} id={`genre-${idx}`}>
                                                     {genre}
+                                                </ul>
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="dropdown">
+                                <h3 className="filter-bar-titles" id="rating-header"
+                                    onClick={() => this.dropDown("rating")}>Average Ratings</h3>
+                                <div className='nav-bar-search-by-genre' id="rating-dropdown">
+                                    <button className="index-book-filter-bar-buttons"
+                                        onClick={() => this.filterBooksByRating(``)}>
+                                        All Ratings
+                                    </button>
+                                    {MinMaxBookRatings(this.props.books).map((rating,idx) => (
+                                        <div key={`${rating}`} className="add-shelves-sidebar-shelf">
+                                            <button className="index-book-filter-bar-buttons"
+                                                onClick={() => this.filterBooksByRating(`${rating}`, `${idx}`)}
+                                            >
+                                                <ul className={`index-book-filter-bar-buttons `} id={`rating-${idx}`}>
+                                                    {rating}
                                                 </ul>
                                             </button>
                                         </div>
